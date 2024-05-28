c40a3a92476ad23587b3c8f373850d843c98797b
Michael Egli <michael.egli@contractors.roche.com>
2024-05-28 10:12:09 +0200

    Add wezterm config.

---
+++ wezterm/README.md
@@ -0,0 +1,9 @@
+# Wezterm configuration
+
+My personal [wezterm](https://wezfurlong.org/wezterm) configuration files.
+
+Clone it to ~/.config/wezterm`:
+
+```bash
+git clone git@github.com:eglimi/wezterm-config.git ~/.config/wezterm
+```
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,139 @@
+local wezterm = require "wezterm"
+local config = wezterm.config_builder()
+local act = wezterm.action
+local mux = wezterm.mux
+
+config.default_prog = { "/bin/zsh" }
+
+front_end = "WebGL"
+
+-- config.font = wezterm.font("Hack Nerd Font Mono")
+config.font = wezterm.font_with_fallback {
+	"Monaspace Neon",
+	{ family = "Monaspace Radon", style = "Italic" },
+}
+config.font_size = 15.0
+
+config.color_scheme = "Gruvbox Dark (Gogh)"
+
+config.colors = {
+	cursor_bg = "#E91E63",
+	tab_bar = {
+		background = "#928374",
+		active_tab = {
+			bg_color = "#D79921",
+			fg_color = "#9D0006",
+		},
+		inactive_tab = {
+			bg_color = "#3C3836",
+			fg_color = "#B57614",
+		}
+	},
+}
+
+config.enable_tab_bar = false
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.window_padding = {left = 0, right = 0, top = 0, bottom = 0}
+config.window_decorations = "TITLE|RESIZE"
+config.window_close_confirmation = "AlwaysPrompt"
+config.scrollback_lines = 5000
+config.audible_bell = "Disabled"
+
+-- keys
+config.disable_default_key_bindings = true
+config.leader = { key = "j", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+
+	-- font size
+	{ key = '=', mods = 'CTRL', action = wezterm.action.IncreaseFontSize },
+	{ key = '-', mods = 'CTRL', action = wezterm.action.DecreaseFontSize },
+	{ key = '0', mods = 'CTRL', action = wezterm.action.ResetFontSize },
+
+	-- copy / paste
+	{ key = "[", mods = "LEADER", action = act.ActivateCopyMode },
+	{ key = "v", mods = "CTRL|SHIFT", action = act.PasteFrom("Clipboard") },
+	{ key = "c", mods = "CTRL|SHIFT", action = act.CopyTo("ClipboardAndPrimarySelection") },
+
+	-- scrolling
+	{ key = "UpArrow", mods = "SHIFT", action = act.ScrollByLine(-10) },
+	{ key = "DownArrow", mods = "SHIFT", action = act.ScrollByLine(10) },
+	{ key = "f", mods = "LEADER", action = act.Search{CaseInSensitiveString=""} },
+	{ key = "s", mods = "LEADER", action = act.QuickSelect },
+
+	-- panes
+	{ key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "=", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "H", mods = "CTRL", action = act.ActivatePaneDirection("Left") },
+	{ key = "J", mods = "CTRL", action = act.ActivatePaneDirection("Down") },
+	{ key = "K", mods = "CTRL", action = act.ActivatePaneDirection("Up") },
+	{ key = "L", mods = "CTRL", action = act.ActivatePaneDirection("Right") },
+	{ key = "x", mods = "LEADER", action = act.CloseCurrentPane { confirm = true } },
+
+
+	-- tabs
+	{ key = "c", mods = "LEADER", action = act.SpawnCommandInNewTab({
+		cwd = "~",
+		domain = "CurrentPaneDomain",
+	})
+	},
+	{ key = "n", mods = "LEADER", action = act.ActivateTabRelative(1) },
+	{ key = "p", mods = "LEADER", action = act.ActivateTabRelative(-1) },
+	{ key = "l", mods = "LEADER", action = act.ActivateLastTab },
+	{ key = "z", mods = "LEADER", action = act.CloseCurrentTab { confirm = true } },
+	{ key = "t", mods = "LEADER", action = act.ShowTabNavigator },
+	-- { key = "RightArrow", mods = "CTRL|SHIFT", action = act.MoveTabRelative(1) },
+	-- { key = "LeftArrow", mods = "CTRL|SHIFT", action = act.MoveTabRelative(-1) },
+}
+
+for i = 1, 9 do
+  -- LEADER + number to activate that tab
+  table.insert(config.keys, { key = tostring(i), mods = "LEADER", action = act.ActivateTab(i - 1) })
+end
+
+table.insert(config.keys, {
+	key = ",", mods = "LEADER",
+	action = act.PromptInputLine {
+		description = 'Enter new name for tab',
+		action = wezterm.action_callback(function(window, pane, line)
+			if line then
+				window:active_tab():set_title(line)
+			end
+		end),
+	}
+})
+
+-- Helper function to get the basename of a path
+function basename(s)
+  return string.gsub(s, '(.*[/\\])(.*)', '%2')
+end
+
+-- Show tab index and info in the tab.
+-- If the tab has a title that set explicitly, show that. Otherwise, show the process name.
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local title = tab.tab_title
+	if title and #title > 0 then
+		local tab_name = wezterm.truncate_right(title, 12)
+		return wezterm.pad_right(' ' .. tab.tab_index+1 .. ': ' .. tab_name .. ' ', 8)
+	end
+	-- local info = wezterm.truncate_right(basename(tab.active_pane.foreground_process_name), 12)
+	-- return wezterm.pad_right(' ' .. tab.tab_index+1 .. ': ' .. info .. ' ', 8)
+	-- For now, just return the index, as it is faster to compute.
+	-- The process name would be useful, but it isn't always there.
+	return wezterm.pad_right(' [' .. tab.tab_index+1 .. '] ', 8)
+end)
+
+-- start maximize. Useful when not working on a tiling wm.
+-- On Ubuntu, use Win-Up / Down to maximize and toggle
+-- wezterm.on('gui-startup', function(cmd)
+--   local tab, pane, window = mux.spawn_window(cmd or {})
+--   window:gui_window():maximize()
+-- end)
+
+-- Use default local domain
+-- config.unix_domains = {
+--   { name = 'unix' },
+-- }
+-- config.default_gui_startup_args = { 'connect', 'unix' }
+
+return config
