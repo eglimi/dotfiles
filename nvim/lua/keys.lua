ddcf454dc09ae4109a180f2e92d641a4fb3bbbd8
Michael Egli <michael.egli@contractors.roche.com>
2024-08-27 09:51:16 +0200

    nvim: Format keys.lua

--- nvim/lua/keys.lua
+++ nvim/lua/keys.lua
@@ -59,29 +59,29 @@ vim.keymap.set("v", "<space>ft", ":'<,'>EasyAlign *|<CR>", { desc = "Align Markd
 vim.keymap.set('t', [[<Esc>]], [[<C-\><C-n>]], { desc = "Escape terminal mode" } )
 
 -- More consistent behaviour of cr
-  local keycode = vim.keycode or function(x)
-    return vim.api.nvim_replace_termcodes(x, true, true, true)
-  end
-  local keys = {
-    ['cr']        = keycode('<CR>'),
-    ['ctrl-y']    = keycode('<C-y>'),
-    ['ctrl-y_cr'] = keycode('<C-y><CR>'),
-  }
+local keycode = vim.keycode or function(x)
+  return vim.api.nvim_replace_termcodes(x, true, true, true)
+end
+local keys = {
+  ['cr']        = keycode('<CR>'),
+  ['ctrl-y']    = keycode('<C-y>'),
+  ['ctrl-y_cr'] = keycode('<C-y><CR>'),
+}
 
-  _G.cr_action = function()
-    if vim.fn.pumvisible() ~= 0 then
-      -- If popup is visible, confirm selected item or add new line otherwise
-      local item_selected = vim.fn.complete_info()['selected'] ~= -1
-      return item_selected and keys['ctrl-y'] or keys['ctrl-y_cr']
-    else
-      -- If popup is not visible, use plain `<CR>`. You might want to customize
-      -- according to other plugins. For example, to use 'mini.pairs', replace
-      -- next line with `return require('mini.pairs').cr()`
-      return keys['cr']
-    end
+_G.cr_action = function()
+  if vim.fn.pumvisible() ~= 0 then
+    -- If popup is visible, confirm selected item or add new line otherwise
+    local item_selected = vim.fn.complete_info()['selected'] ~= -1
+    return item_selected and keys['ctrl-y'] or keys['ctrl-y_cr']
+  else
+    -- If popup is not visible, use plain `<CR>`. You might want to customize
+    -- according to other plugins. For example, to use 'mini.pairs', replace
+    -- next line with `return require('mini.pairs').cr()`
+    return keys['cr']
   end
+end
 
-  vim.keymap.set('i', '<CR>', 'v:lua._G.cr_action()', { expr = true })
+vim.keymap.set('i', '<CR>', 'v:lua._G.cr_action()', { expr = true })
 
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
