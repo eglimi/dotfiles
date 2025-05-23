c626268f6e897ac18d2dfe31c6aab7323ab471aa
Michael Egli <michael.egli@contractors.roche.com>
2025-05-23 07:31:03 +0200

    Remove file size restriction for treesitter.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -17,13 +17,6 @@ now(function()
 		highlight = {
 			enable = true,
 			additional_vim_regex_highlighting = false,
-			disable = function(lang, buf)
-				local max_filesize = 200 * 1024 -- 200 KB
-				local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-				if ok and stats and stats.size > max_filesize then
-					return true
-				end
-			end,
 		},
 		indent = { enable = true },
 		incremental_selection = {
