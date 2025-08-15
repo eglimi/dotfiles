a79455dfa0901580d1cacba8a83455846b95078e
Michael Egli <michael.egli@contractors.roche.com>
2025-08-15 14:40:30 +0200

    nvim: Simplify config setup.
    Move all the files into `plugin` folder. This ensures that everything is
    loaded automatically, no `require(...)` is needed anymore.
    
    For files in `plugin`, they are loaded alphabetically. To ensure that
    plugins are loaded first, the file is prefixed with 1_. Other files
    should not have any dependencies.
    
    This also removes init.lua, which is now moved mostly to config.lua.

--- nvim/plugin/1_plugins.lua
+++ nvim/plugin/1_plugins.lua
@@ -1,4 +1,21 @@
 -- We use mini.deps for dependency management
+
+-- Clone 'mini.nvim' manually in a way that it gets managed by 'mini.deps'
+local path_package = vim.fn.stdpath('data') .. '/site/'
+local mini_path = path_package .. 'pack/deps/start/mini.nvim'
+if not vim.loop.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  local clone_cmd = {
+    'git', 'clone', '--filter=blob:none',
+    'https://github.com/echasnovski/mini.nvim', mini_path
+  }
+  vim.fn.system(clone_cmd)
+  vim.cmd('packadd mini.nvim | helptags ALL')
+end
+
+-- Set up 'mini.deps' (customize to your liking)
+require('mini.deps').setup({ path = { package = path_package } })
+
 local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
 
 now(function()
@@ -146,8 +163,6 @@ You must
 			},
 		})
 	end
-
-	require('lsp')
 end)
 
 later(function()
@@ -162,9 +177,9 @@ end)
 
 later(function()
 	add('tpope/vim-fugitive')
-	require("fugitive-handler")
 	add('junegunn/gv.vim')
 	add('yorickpeterse/nvim-window')
+	add('martintrojer/jj-fugitive')
 end)
 
 now(function()
@@ -173,3 +188,27 @@ now(function()
 	add('nmac427/guess-indent.nvim')
 	require('guess-indent').setup({})
 end)
+
+-- Colorscheme
+now(function()
+  -- Some favorites
+	-- add("neanias/everforest-nvim")
+	-- require("everforest").setup({ background = "hard", italics = true,  })
+	-- add("sainnhe/gruvbox-material")
+	add( { source = "everviolet/nvim", name = "evergarden" })
+	require("evergarden").setup({
+		theme = {variant = "spring"}, -- spring, summer, fall, winter
+		style = {search = {"standout"}, incsearch = {"standout", "italic"}, comment = {}},
+		editor = {float = {invert_border = false}},
+		overrides = function(colors)
+			return {
+				WinSeparator = {fg = colors.blue},
+				WinBar = {style = {"italic", "bold"}},
+			}
+		end,
+	})
+
+	vim.o.background = "dark"
+  vim.cmd('colorscheme evergarden')
+end)
+
---
+++ nvim/plugin/config.lua
@@ -0,0 +1,43 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+
+vim.opt.clipboard = "unnamed"
+
+-- Global options
+vim.opt.termguicolors = true
+vim.opt.inccommand = 'split'
+vim.opt.scrolloff = 2
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.colorcolumn = "100"
+vim.opt.signcolumn = "yes"
+vim.opt.foldcolumn = "0"
+vim.opt.breakindent = true
+
+vim.opt.list = true
+vim.opt.listchars = "tab:▸.,lead:›,trail:×"
+
+vim.opt.tabstop = 4
+--vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.undofile = true
+
+vim.opt.wrap = true
+
+vim.opt.mouse = ""
+-- Disable netrw, we use oil
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- statusbar and winbar
+-- Use the global statusbar and show the filename in the winbar
+vim.opt.laststatus = 3
+vim.opt.winbar = '%=%m %t%='
+
+vim.opt.completeopt = "menuone,noselect,popup"
+
+vim.opt.winborder = "rounded"
+
