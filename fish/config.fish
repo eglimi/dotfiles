6369b4d86fc6f057454c2a99578ea968be42a06b
Michael Egli <michael.egli@contractors.roche.com>
2025-11-09 13:54:34 +0100

    fish: Update prompt (no longer use starship)
    The fish prompt is very capable already. Also, I can add a
    jj prompt function easily, which starship still doesn't support.
    
    All other starship functionality is not super interesting for me
    anyway.

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,7 @@ set -g fish_history_max 10000
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
-    starship init fish | source
+    # starship init fish | source
 	fzf --fish | source
     if command -q mise
         mise activate fish | source
---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,34 @@
+function fish_prompt --description 'Write out the prompt'
+        set -l last_pipestatus $pipestatus
+        set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+
+        set color_cwd $fish_color_cwd
+        set suffix ' $'
+
+        # PWD
+        set_color $color_cwd
+        echo -n (prompt_pwd)
+        set_color normal
+
+        set -l status_color (set_color $fish_color_status)
+        set -l statusb_color (set_color --bold $fish_color_status)
+        set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+        echo -n $prompt_status
+        set_color normal
+
+        echo -n "$suffix "
+end
+
+function fish_right_prompt --description 'My custom prompt, right side'
+        set -g __fish_git_prompt_show_informative_status 1
+        set -g __fish_git_prompt_hide_untrackedfiles 1
+        set -g __fish_git_prompt_color_branch magenta --bold
+        set -g __fish_git_prompt_showupstream informative
+        set -g __fish_git_prompt_color_dirtystate blue
+        set -g __fish_git_prompt_color_stagedstate yellow
+        set -g __fish_git_prompt_color_invalidstate red
+        set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+        set -g __fish_git_prompt_color_cleanstate green --bold
+
+        printf '%s ' (fish_vcs_prompt)
+end
