84806d9d5ece0eaa025763d3e7a85e71554e9979
Michael Egli <michael.egli@contractors.roche.com>
2025-11-18 08:16:06 +0100

    fish: Don't include podman path.
    Also include a script to remove a path from the universal
    variables.

--- fish/conf.d/00-environment.fish
+++ fish/conf.d/00-environment.fish
@@ -1,12 +1,12 @@
 fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/programs/zig
-fish_add_path $HOME/programs/podman/bin
 fish_add_path $HOME/.local/bin
 fish_add_path /usr/local/go/bin
 fish_add_path $HOME/go/bin
 
 # Environment variables
 set -gx EDITOR editor
+set -gx SHELL fish
 set -gx LESS '--quit-if-one-screen --ignore-case --chop-long-lines --RAW-CONTROL-CHARS --no-init --tabs=4 --shift=4'
 
 if test -f $HOME/.env
