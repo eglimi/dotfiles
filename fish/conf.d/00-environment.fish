40e9016c4b86f16664a26004e344f2a663743303
Michael Egli <michael.egli@contractors.roche.com>
2026-05-08 09:25:07 +0200

    fish: Set nvim explicitly as editor.

--- fish/conf.d/00-environment.fish
+++ fish/conf.d/00-environment.fish
@@ -2,10 +2,9 @@ fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/programs/zig
 fish_add_path $HOME/.local/bin
 fish_add_path /usr/local/go/bin
-fish_add_path $HOME/go/bin
 
 # Environment variables
-set -gx EDITOR editor
+set -gx EDITOR nvim
 set -gx SHELL fish
 set -gx LESS '--quit-if-one-screen --ignore-case --chop-long-lines --RAW-CONTROL-CHARS --no-init --tabs=4 --shift=4'
 set -gx RIPGREP_CONFIG_PATH $HOME/.config/ripgreprc
