0a6988fa34a850f972fec6ace91ed8c3c5e72c61
Michael Egli <michael.egli@contractors.roche.com>
2026-06-24 08:44:46 +0200

    fish: cleanup startup tools and environment.

--- fish/config.fish
+++ fish/config.fish
@@ -3,11 +3,8 @@ set -g fish_history_max 10000
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
-    # starship init fish | source
 	fzf --fish | source
-    if command -q mise
-        mise activate fish | source
-    end
+    zoxide init fish | source
     atuin init fish --disable-up-arrow | source
 
     source $HOME/.aliases
